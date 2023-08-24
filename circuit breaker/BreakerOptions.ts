448decae153efcf6c096fd7956f94bb7cf2904cd
I545672 <yasachandra.bansal@sap.com>
2023-08-24 11:31:51 +0530

    Final commit

---
+++ circuit breaker/BreakerOptions.ts
@@ -0,0 +1,8 @@
+export class BreakerOptions { 
+    constructor
+    (
+        public failureThreshold: number,
+        public successThreshold: number,
+        public timeout: number
+    ){}
+}
